1878cf103c3261a57fd2307f81f09ff4289229ce
Waldir Marques <waldirmarquesdesouza@gmail.com>
2022-06-10 10:29:03 -0300

    first commit

---
+++ src/test/java/br/com/taskcontrol/TaskcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.taskcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
